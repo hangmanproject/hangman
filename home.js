620e2465127e1e450c5b8f05104cb1ccd7be185b
Brigitte Huneke <brigittehuneke@gmail.com>
2016-12-06 11:15:46 -0800

    updated top players table

--- home.js
+++ home.js
@@ -3,6 +3,7 @@
 var players = [];
 var allPlayerNames  = [];
 var storedPlayersString = localStorage.getItem('players');
+var storedNamesString = localStorage.getItem('allPlayerNames');
 
 function PlayerInfo(name) {
   this.playerName = name;
@@ -13,8 +14,8 @@ function PlayerInfo(name) {
   this.ranking = 0;
 };
 
-
 retrieveLocal();
+
 
 var playerForm = document.getElementById('form');
 
@@ -25,24 +26,31 @@ function handleSubmit(event){
 
   var playerName = event.target.player_name.value;
   var playerNameUpper = playerName.toUpperCase();
+  var totalPoints = event.target.total_points.value;
 
   checkName();
-
 
   function checkName() {
     if (players.length === 0) {
       var newPlayer = new PlayerInfo(playerNameUpper);
-      players.ranking = 1;
+      newPlayer.ranking = 1;
+      newPlayer.totalPoints = totalPoints;
       players.push(newPlayer);
       allPlayerNames.push(playerNameUpper);
+      renderTopPlayerRow(newPlayer);
       storeLocal();
       return writeWelcomeMessage('Welcome ' + playerName + '!');
     } else {
-      if (allPlayerNames.indexOf(playerNameUpper) !== -1) {
+      var playerIndex = allPlayerNames.indexOf(playerNameUpper);
+      if (playerIndex !== -1) {
+        players[playerIndex].totalPoints = totalPoints;
+        sortPlayers();
+        renderTable();
         return writeWelcomeMessage('Welcome back, ' + playerName + '!');
-      } else if (allPlayerNames.indexOf(playerNameUpper) === -1) {
+      } else if (playerIndex === -1) {
         newPlayer = new PlayerInfo(playerNameUpper);
         players.push(newPlayer);
+        newPlayer.totalPoints = totalPoints;
         allPlayerNames.push(playerNameUpper);
         sortPlayers();
         renderTable();
@@ -63,98 +71,76 @@ function writeWelcomeMessage(message) {
 
 function sortPlayers(){
   players.sort(function(a, b) {
-    return (b.playerName) - (a.playerName);
+    return (b.totalPoints) - (a.totalPoints);
   });
 };
 
-// function sortPlayers(){
-//   players.sort(function(a, b) {
-//     return (b.totalPoints) - (a.totalPoints);
-//   });
-// };
-
 function storeLocal() {
   var playersJSON = JSON.stringify(players);
+  var namesJSON = JSON.stringify(allPlayerNames);
   localStorage.setItem('players', playersJSON);
+  localStorage.setItem('allPlayerNames', namesJSON);
 };
 
 function retrieveLocal() {
   if (storedPlayersString) {
     players = JSON.parse(storedPlayersString);
+    allPlayerNames = JSON.parse(storedNamesString);
+    renderTable();
   }
 };
 
 function renderTable(){
-  for (var i = 0; i < 5; i++) {
-    if (players[i].playerName !== 'Ron') {
-      renderTopPlayerRow();
-    }
+  clearTable();
+  var rank = 0;
+  var playersLength = players.length;
+  if (playersLength > 5) {
+    playersLength = 5;
+  }
+  for (var i = 0; i < playersLength; i++) {
+    rank = rank + 1;
+    players[i].ranking = rank;
+    console.log(players[i]);
+    renderTopPlayerRow(players[i]);
   }
 };
 
-// function renderTable(){
-//   var rank = 0;
-//   for (var i = 0; i < 5; i++) {
-//     rank = rank + 1;
-//     players[i].ranking = rank;
-//     renderTopPlayerRow();
-//   }
-// };
-
-function renderTopPlayerRow() {
+function renderTopPlayerRow(newPlayer) {
   var topPlayersTable = document.getElementById('table_body');
   var tableRow = document.createElement('tr');
-  // var rankingTop = document.createElement('td');
+  var rankingTop = document.createElement('td');
   var playerNameTop = document.createElement('td');
   var gamesPlayedTop = document.createElement('td');
   var gamesWonTop = document.createElement('td');
   var percentageWonTop = document.createElement('td');
   var totalPointsTop = document.createElement('td');
 
-    // rankingTop.textContent = players[i].ranking;
-    // tableRow.appendChild(rankingTop);
+  rankingTop.textContent = newPlayer.ranking;
+  tableRow.appendChild(rankingTop);
 
-  playerNameTop.textContent = players.playerName;
+  playerNameTop.textContent = newPlayer.playerName;
   tableRow.appendChild(playerNameTop);
 
-  gamesPlayedTop.textContent = players.gamesPlayed;
+  gamesPlayedTop.textContent = newPlayer.gamesPlayed;
   tableRow.appendChild(gamesPlayedTop);
 
-  gamesWonTop.textContent = players.gamesWon;
+  gamesWonTop.textContent = newPlayer.gamesWon;
   tableRow.appendChild(gamesWonTop);
 
-  percentageWonTop.textContent = players.percentWon;
+  percentageWonTop.textContent = newPlayer.percentWon;
   tableRow.appendChild(percentageWonTop);
 
-  totalPointsTop.textContent = players.totalPoints;
+  totalPointsTop.textContent = newPlayer.totalPoints;
   tableRow.appendChild(totalPointsTop);
 
   topPlayersTable.appendChild(tableRow);
 }
 
-
-
-//   Pseudocoding:
-//   Player name:
-//   - user to enter chosen name
-//     - set to uppercase/lowercase (check on if numbers/characteristics are ok)
-//     - loop through local storage to see if chosen name has been used before
-//       - if chosen doesn't exist, create new player object
-//       - if chosen name does exist, load previously stored information for that name
-//     - possibly add message to welcome new or old player
-//   Select game:
-//   - user to select game to play
-//     - if user has not entered player name
-//       - stay on page and alert them to log in
-//     - if user has entered name
-//       - user to move to the game page
-// Top player stats:
-// - to display
-//   - ranking
-//   - player name
-//   - total points
-// - show top 5 players in list in order of total points
-
+function clearTable(){
+  var topPlayersTable = document.getElementById('table_body');
+  topPlayersTable.textContent = '';
+}
+//=========================================
 
 //HARDCODED TEST PLAYERS
 // function PlayerInfo(name, won, played, percent, total) {
@@ -189,3 +175,24 @@ function renderTopPlayerRow() {
 // var playerSix = new PlayerInfo('August', 7, 21, '33%', 35);
 // players.push(playerSix);
 // sortPlayers();
+
+//   Pseudocoding:
+//   Player name:
+//   - user to enter chosen name
+//     - set to uppercase/lowercase (check on if numbers/characteristics are ok)
+//     - loop through local storage to see if chosen name has been used before
+//       - if chosen doesn't exist, create new player object
+//       - if chosen name does exist, load previously stored information for that name
+//     - possibly add message to welcome new or old player
+//   Select game:
+//   - user to select game to play
+//     - if user has not entered player name
+//       - stay on page and alert them to log in
+//     - if user has entered name
+//       - user to move to the game page
+// Top player stats:
+// - to display
+//   - ranking
+//   - player name
+//   - total points
+// - show top 5 players in list in order of total points
