1d7fd8e1be0797b354f8288dc03b5c22b42a8ab5
cmd-kvn <kvn.m.wong@gmail.com>
2016-12-07 14:30:49 -0800

    displayed remaining guesses

--- app.js
+++ app.js
@@ -114,6 +114,7 @@ var wonGame = false; // used in checkForWin/Loss and updatePlayerStats
 var letterButton = document.getElementById('letter_buttons');
 letterButton.addEventListener('click', handleClick); // listens for a button click
 var display = document.getElementById('display_player_array');
+var displayGuessesRemaining = document.getElementById('display_guesses_remaining');
 var playAgainButton = document.getElementById('play_again_btn');
 playAgainButton.addEventListener('click', handlePlayAgain);
 var endMessage = document.getElementById('end_of_game_msg');
@@ -123,6 +124,11 @@ var endMessage = document.getElementById('end_of_game_msg');
 // _______ADDRESS______________________
 
 // FUNCTIONS // FUNCTIONS // FUNCTIONS // FUNCTIONS // FUNCTIONS // FUNCTIONS // FUNCTIONS //
+function updateGuessesRemaining() {
+  var guessesRemaining = maxEasyGuesses - incorrectGuesses;
+  displayGuessesRemaining.textContent = 'Remaining guesses: ' + guessesRemaining;
+}
+
 // handles the event when 'play again' button is clicked
 function handlePlayAgain(event) {
   // reloads the page and starts it at the top
@@ -145,6 +151,7 @@ function handleClick(event) {
         playerAnswerArr[i] = gameWord[i]; // replace the blank index with the matching letter
 
         displayPlayerArray(playerAnswerArr); // update the display for the player
+        updateGuessesRemaining();
         remainingLetters--;
         // disable the button to prevent the player from selecting it again
         event.target.disabled = true;
@@ -158,6 +165,7 @@ function handleClick(event) {
     //       - one body part gets added to the hangman
 
     incorrectGuesses += 1; // incorrectGuesses increases by 1
+    updateGuessesRemaining();
     // disable the button to prevent the player from selecting it again
     event.target.disabled = true;
     checkForLoss();
@@ -257,6 +265,7 @@ function runGame(){
   // generate a playerAnswerArr blank puzzle that is the length of the gameWord and display it
   generatePlayerAnswerArray(gameWord);
   displayPlayerArray(playerAnswerArr);
+  updateGuessesRemaining();
 }
 
 //*****EXECUTE CODE*******************EXECUTE CODE**********************
